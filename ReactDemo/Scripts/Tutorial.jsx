3d54793dba3a1f3124206c6450a5324ad2a9d379
Owen <owenpyrah@hotmail.com>
2016-07-01 16:20:21 +0100

    Finished, needs bug fixing on comment submit

--- ReactDemo/Scripts/Tutorial.jsx
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -9,19 +9,35 @@ var CommentBox = React.createClass({
     }.bind(this);
     xhr.send();
   },
+
+  handleCommentSubmit: function (comment) {
+      var data = new FormData();
+      data.append('Author', comment.Author);
+      data.append('Text', comment.Text);
+
+      var xhr = new XMLHttpRequest();
+      xhr.open('post', this.props.submitUrl, true);
+      xhr.onload = function () {
+          this.loadCommentsFromServer();
+      }.bind(this);
+      xhr.send(data);
+  },
+
   getInitialState: function() {
     return {data: []};
   },
+
   componentDidMount: function() {
     this.loadCommentsFromServer();
     window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
+
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+         <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
@@ -58,17 +74,32 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+    handleSubmit: function (e) {
+        e.preventDefault();
+        var author = this.refs.author.value.trim();
+        var text = this.refs.text.value.trim();
+        if (!text || !author) {
+            return;
+        }
+        this.props.onCommentSubmit({Author: author, Text: text});
+        this.refs.author.value = '';
+        this.refs.text.value = '';
+        return;
+    },
+
     render: function () {
         return (
-          <div className="commentForm">
-              Hello, world! I am a CommentForm.
-          </div>
+           <form className="commentForm">
+        <input type="text" placeholder="Your name" ref="author"/>
+        <input type="text" placeholder="Say something..." ref="text"/>
+        <input type="submit" value="Post" />
+      </form>
       );
     }
 });
 
 ReactDOM.render(
-  <CommentBox url="/comments" pollInterval={2000} />,
+  <CommentBox url="/comments" submitUrl="/comments/new" pollInterval={2000} />,
   document.getElementById('content')
 );
 
