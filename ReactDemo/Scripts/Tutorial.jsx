dfd2cfdbc15b4cf85b11292c17456ea924e4f40e
Owen <owenpyrah@hotmail.com>
2016-07-01 16:05:09 +0100

    Add project files.

---
+++ ReactDemo/Scripts/Tutorial.jsx
@@ -0,0 +1,74 @@
+﻿
+var CommentBox = React.createClass({
+  loadCommentsFromServer: function() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', this.props.url, true);
+    xhr.onload = function() {
+      var data = JSON.parse(xhr.responseText);
+      this.setState({ data: data });
+    }.bind(this);
+    xhr.send();
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadCommentsFromServer();
+    window.setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="commentBox">
+        <h1>Comments</h1>
+        <CommentList data={this.state.data} />
+        <CommentForm />
+      </div>
+    );
+  }
+});
+
+
+
+var CommentList = React.createClass({
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        <Comment author={comment.Author}>
+            {comment.Text}
+        </Comment>
+      );
+    });
+    return (
+      <div className="commentList">{commentNodes}
+      </div>
+    );
+  }
+});
+var Comment = React.createClass({
+    render: function () {
+        return (
+          <div className="comment">
+            <h2 className="commentAuthor">
+                {this.props.author}
+            </h2>
+              {this.props.children}
+          </div>
+      );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function () {
+        return (
+          <div className="commentForm">
+              Hello, world! I am a CommentForm.
+          </div>
+      );
+    }
+});
+
+ReactDOM.render(
+  <CommentBox url="/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
+
